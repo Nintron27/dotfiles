de7c39f0f7f6a0b0b2b05030384643abc9bf7f3d
Nintron <nintron@sent.com>
2024-10-21 22:01:19 -0400

    Remove Argentum specific home files

--- homes/argentum/common/helix/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  programs.helix = {
-    enable = true;
-    # catppuccin.enable = true; # Done below instead
-  };
-  home.file.".config/helix" = {
-    source = ./config;
-  };
-}
--- homes/argentum/common/zellij/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  programs.zellij = {
-    enable = true;
-    # catppuccin.enable = true; #Done below instead
-  };
-  home.file.".config/zellij/config.kdl" = {
-    source = ./config.kdl;
-  };
-}
