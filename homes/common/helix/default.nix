bbde2d11b087b288a7e8d69f3e0d1edc8df6ceae
Nintron <nintron@sent.com>
2025-05-14 20:35:01 -0400

    Use helix from master

--- homes/common/helix/default.nix
+++ homes/common/helix/default.nix
@@ -1,6 +1,7 @@
-{
+{ inputs, ... }: {
   programs.helix = {
     enable = true;
+    package = inputs.helix.packages."x86_64-linux".default;
     # catppuccin.enable = true; # Done below instead
   };
   home.file.".config/helix" = {
